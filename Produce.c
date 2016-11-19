772e756c54a437b93aea6f5dab1e590af1ddda2b
Ramiro Pinedo <rpinedo@ufl.edu>
2016-11-18 23:33:12 -0500

    v.2

--- Produce.c
+++ Produce.c
@@ -1,7 +1,20 @@
 #include<stdio.h>
 #include<stdbool.h>
 
+float rem(float w, float tot, float a[], float b[]){
 
+	float off = 0.0;
+	int i =0;
+		printf("weight value : %.2f\n", w);
+	while ( w != a[i]){
+	i++;
+	}
+	printf("index value : %d\n", i);
+	off = b[i]*w;
+	
+	printf("off value : %.2f\n", off);
+return off;
+}
 
 int main(){
 
@@ -10,22 +23,47 @@ int main(){
 bool loop =true;
 char select;
 float price = 0.0, weight = 0.0, total =0.0;
-
-
+float a[500] ,b[500];
+int i=0;
 // 
 
 while(loop == true){
+
 	printf("Make a selection: (a)dd, (r)emove, (t)otal, or (q)uit. ");
 		scanf(" %c", &select);
+		
+	if(select != 'a' && select != 'r' && select != 't' && select != 'q'){
+		
+		printf("Invalid entry, try again.\n");
+	}
+
 
 // ADD CONDITION
-	if(select == 'a'){
-		printf("Enter a Price/Pound: ");
-			scanf(" %f", &price);
-		printf("Enter a Weight: ");
-			scanf(" %f", &weight);
+	else if(select == 'a'){
 	
-	
+		//bool cond = true;
+		//while( cond == true){
+			printf("Enter a Price/Pound: ");
+				scanf(" %f", &price);
+				
+				
+						
+				
+				//if (price < 0 || price != ){
+				
+					//printf("Invalid entry.\n");
+				//}
+				//  else cond = false;
+		//}
+				
+			printf("Enter a Weight: ");
+				scanf(" %f", &weight);
+		
+			total += (weight*price);
+			
+			a[i] = price;
+			b[i] = weight;
+		i++;
 	}
 	
 // REMOVE CONDITION
@@ -34,16 +72,12 @@ while(loop == true){
 		printf("Enter a Weight: ");
 			scanf(" %f", &weight);
 	
+		total = total - rem(weight,total, a,b);
 	}
 
 // TOTAL CONDITION
 	else if(select == 't'){
-		
-		
-		
-		//total = total();
-		
-		
+	
 		printf("The current total is: $%.2f.", total);
 			
 	}
@@ -54,9 +88,13 @@ while(loop == true){
 	}
 
 printf("\n");
-
+printf("array 1: %.2f\n", a[i-1]);
+printf("array 2: %.2f\n", b[i-1]);
 }
 
 
 return 0;
 }
+
+
+
